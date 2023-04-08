15f9d35e1e56b597b1dd5e8eea35a3c7619b1b94
aniket-magadum-fyntune <aniketmagadum77@gmail.com>
2023-04-08 18:09:17 +0530

    Initial Commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AniketMagadum\LogPlus;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
